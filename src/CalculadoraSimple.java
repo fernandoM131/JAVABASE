45f44b57cc6eace386b50a531d7c12e54c4cc235
mabell taleno <mabell.taleno782@std.uni.edu.ni>
2025-06-25 23:38:41 -0600

    Agregada función de multiplicacion y corregido espacio entre métodos

--- src/CalculadoraSimple.java
+++ src/CalculadoraSimple.java
@@ -2,26 +2,41 @@ import java.util.Scanner;
 
 public class CalculadoraSimple {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingrese el primer número:");
+
         int a = scanner.nextInt();
 
         System.out.println("Ingrese el segundo número:");
+
         int b = scanner.nextInt();
 
         // Suma
         int suma = a + b;
+
         System.out.println("Suma: " + suma);
 
         // Resta
         int resta = a - b;
+
         System.out.println("Resta: " + resta);
 
        // Multiplicación
         int multiplicacion = a * b;
-        System.out.println("Multiplicación: " + multiplicacion);
- 
+
+        System.out.println("Multiplicación: " + multiplicacion); 
+
+        // División
+        if (b != 0) {
+            float division = (float) a / b;
+            System.out.println("División: " + division);
+
+        } else {
+
+            System.out.println("División: No se puede dividir por cero");
+        }
         scanner.close();
 
     }
